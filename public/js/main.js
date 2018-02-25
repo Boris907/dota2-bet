fb77f278fabadc0330992476c3d370cbde9d0ad9
Bobchik <piton1936@gmail.com>
2018-02-25 12:46:33 +0200

    webmoney pay value fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
+    var wm = $("script[id='wm-script']").attr("src");
+    var amount = wm.split('&')[3];
+    var val = amount.split('=')[1];
+    var web_money = $('#web_money').val();
+    var res = amount.replace(val, web_money);
+    var result = wm.replace("amount=0", res);
+
+    $('#wm-script').attr({
+        src: result,
+        type: 'text/javascript'
+    }).appendTo('body');
+
     $('.bet_submit').click(function (e) {
         e.preventDefault();
         var bet = $(this).attr("value");
         $('.increase').prop('checked', false);
-        $('#exampleModal').modal('hide');
 
+        $('#exampleModal').modal('hide');
         $.ajax({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
--- resources/views/checkout/webmoney.blade.php
+++ resources/views/checkout/webmoney.blade.php
@@ -6,9 +6,15 @@
             <h1 class="text-center">Webmoney checkout</h1>
             <img class="img-responsive col-xs-offset-4" src="{{'/04.jpg'}}" width="150px">
             <br>
+            <h4 class="text-center">Money</h4>
+            <div class="col-md-4 col-md-offset-4">
+            <input type="text" id="web_money" class="form-control" value="0">
+            </div>
+            <br>
+            <br>
             <div class="col-md-offset-1">
-                <script src="//merchant.webmoney.ru/conf/lib/wm-simple-x20.min.js?wmid=396850847264&purse=R251053037627&key=398146463&amount=10&desc=Покупка валюты"
-                        id="wm-script"></script>
+                <script src="//merchant.webmoney.ru/conf/lib/wm-simple-x20.min.js?wmid=396850847264&purse=R251053037627&key=398146463&amount=0&desc=Покупка валюты"
+                        id="wm-script"> </script>
             </div>
         </div>
     </div>
